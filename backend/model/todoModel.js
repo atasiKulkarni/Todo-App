52a2e74372eb89ea7e88c25df377e7fcfea5258e
Atasi Kulkarni <75190043+atasikulkarni@users.noreply.github.com>
2021-06-26 14:04:07 +0530

    Add files via upload

--- backend/model/todoModel.js
+++ backend/model/todoModel.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
+const TodoSchema = mongoose.Schema(
+    {
+       currentState: String,
+       Title: String,
+       Description: String,
+       CreatedDate: Date,
+       DueDate: Date,
+       Priority: String,
+      
 
+    },
+    {collection: "ToDoApp"}
+);
+module.exports = mongoose.model("ToDoApp", TodoSchema);
